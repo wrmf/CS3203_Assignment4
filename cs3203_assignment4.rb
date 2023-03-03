eab4b8948acdb0b1ccfb59aa8237d4f16f2036cd
wrmf <31448920+wrmf@users.noreply.github.com>
2023-03-03 16:13:41 -0600

    fixed names and documentation

--- cs3203_assignment4.rb
+++ cs3203_assignment4.rb
@@ -1,7 +1,12 @@
-def check_add(array, key)
-  for num in array #Loop through array
-    for num2 in array #Loop through array again (yes this is O(n^2) time complexity)
-      if num+num2 == key #Check if the two numbers added are equal to the key
+=begin
+Assignment 4 for CS3203
+@author wrmf
+=end
+
+def check_add(numbers, target)
+  for num in numbers #Loop through numbers array
+    for num2 in numbers #Loop through numbers array again (yes this is O(n^2) time complexity)
+      if num+num2 == target #Check if the two numbers added are equal to the target
         return true #Return on true
       end
     end
@@ -10,4 +15,4 @@ def check_add(array, key)
   false #Return false if it was never true
 end
 
-puts check_add([1, -1, 3, 4], 0)
+puts check_add([1, -1, 3, 4], 0) #Example use checking to see if anything in [1, -1, 3, 4] adds up to 0
